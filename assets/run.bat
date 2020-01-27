b3ac20167f26f203ce4b2dc1dcad4ea3737fc764
XhstormR <10527522+XhstormR@users.noreply.github.com>
2020-01-27 19:53:09 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- assets/run.bat
+++ assets/run.bat
@@ -1,4 +1,5 @@
 curl https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/x86_64/mingw64.db -o mingw64.tar.gz
-busybox mkdir -p mingw64 & busybox gzip -d -c mingw64.tar.gz | busybox tar x -C mingw64
+busybox mkdir -p mingw64 & busybox tar -xf mingw64.tar.gz -C mingw64
 
 :: java -jar pacman.jar D:\Download\mingw64 https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/x86_64/ gcc
+:: busybox find -name *.xz -exec busybox tar -xf {} ;
